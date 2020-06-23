56513ec92b5086691861121eaba4dc490cf8f988
Tony Troeff <tony13troev@gmail.com>
2020-06-23 23:31:01 +0300

    Fixing wrong package json arguments causing no files being exported.

---
+++ src/SingleValueInputElements/index.ts
@@ -0,0 +1,6 @@
+export * from './ISingleValueInputElement';
+export * from './ISingleValueInputElementProps';
+export * from './SingleValueInputElement';
+export * from './ISingleInputElementConfiguration';
+
+export * from './TextInputElement/index';
