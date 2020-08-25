01f688aa3c5ea3558979320cf1acec67a00d7d7f
Tony Troeff <tony13troev@gmail.com>
2020-08-25 14:16:33 +0300

    Syncing the dropdown helper class.

---
+++ src/Utilities/DropdownHelper.ts
@@ -0,0 +1,54 @@
+import { DropdownOptionType, IDropdownInputOption } from '../SingleValueInputElements/DropdownInputElement/IDropdownInputOption';
+import { IDropdownOption, SelectableOptionMenuItemType } from 'office-ui-fabric-react';
+
+export class DropdownHelper {
+    public static getNormalizedOptions(defaultOption: IDropdownInputOption, allOptions: IDropdownInputOption[]): IDropdownOption[] {
+        const options = [];
+
+        if (!!defaultOption) {
+            const predefinedDefaultOption: IDropdownOption = {
+                ...DropdownHelper.toNormalizedDropdownOption(defaultOption),
+                selected: true
+            };
+            options.push(predefinedDefaultOption);
+        }
+
+        if (!!allOptions)
+            allOptions.forEach((o): void => {
+                if (!o) return;
+
+                const newOption: IDropdownOption = {
+                    ...DropdownHelper.toNormalizedDropdownOption(o),
+                    selected: false
+                };
+                options.push(newOption);
+            });
+
+        return options;
+    }
+
+    public static toNormalizedDropdownOption = (inputOption: IDropdownInputOption): IDropdownOption => {
+        if (!inputOption) return null;
+
+        let itemType: SelectableOptionMenuItemType;
+        if (inputOption.type !== undefined) {
+            switch (inputOption.type) {
+                case DropdownOptionType.Normal:
+                    itemType = SelectableOptionMenuItemType.Normal;
+                    break;
+                case DropdownOptionType.Divider:
+                    itemType = SelectableOptionMenuItemType.Divider;
+                    break;
+                case DropdownOptionType.Header:
+                    itemType = SelectableOptionMenuItemType.Header;
+                    break;
+            }
+        }
+
+        return {
+            text: inputOption.text,
+            key: inputOption.key,
+            itemType: itemType
+        };
+    };
+}
