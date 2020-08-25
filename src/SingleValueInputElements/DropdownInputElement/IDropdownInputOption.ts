9b5bddec5626c58ff2205db36d4ca8dc4cd68efa
Tony Troeff <tony13troev@gmail.com>
2020-08-25 14:15:40 +0300

    Adding the multi value dropdown.

--- src/SingleValueInputElements/DropdownInputElement/IDropdownInputOption.ts
+++ src/SingleValueInputElements/DropdownInputElement/IDropdownInputOption.ts
@@ -1,4 +1,11 @@
 export interface IDropdownInputOption {
     key: string;
     text: string;
+    type?: DropdownOptionType;
+}
+
+export enum DropdownOptionType {
+    Normal = 0,
+    Divider = 1,
+    Header = 2
 }
---
+++ src/SingleValueInputElements/DropdownInputElement/MultiValueDropdownInput.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Dropdown, IDropdownOption } from 'office-ui-fabric-react';
+import { DropdownHelper } from '../../Utilities/DropdownHelper';
+import { IDropdownInputProps } from './IDropdownInputProps';
+import { ISingleValueInputElementProps } from '../ISingleValueInputElementProps';
+
+export class MultiValueDropdownInput extends React.Component<ISingleValueInputElementProps<string[]> & IDropdownInputProps> {
+    public render(): JSX.Element {
+        const normalizedOptions = DropdownHelper.getNormalizedOptions(this.props?.defaultOption, this.props?.options);
+
+        return (
+            <Dropdown
+                label={this.props?.label}
+                options={normalizedOptions}
+                onChange={(_event: React.FormEvent<HTMLDivElement>, option: IDropdownOption): void => {
+                    const optionKey = option?.key?.toString();
+                    if (!optionKey) return;
+
+                    let currentValues = this.props?.value ?? [];
+
+                    // If the current option is selected, add it to the list.
+                    // Else, remove it.
+                    if (option.selected) currentValues.push(optionKey);
+                    else currentValues = currentValues.filter((x): boolean => x !== optionKey);
+
+                    !!this.props?.onChange && this.props.onChange(currentValues);
+                }}
+                errorMessage={this.props?.errorMessage}
+                required={!!this.props?.isRequired}
+                placeholder={this.props?.placeholder}
+                // This value should never be `undefined`.
+                defaultSelectedKeys={this.props?.value || [this.props?.defaultOption?.key] || null}
+                disabled={this.props?.isDisabled}
+                multiSelect
+            />
+        );
+    }
+}
