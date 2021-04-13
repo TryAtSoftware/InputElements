f3167b18436c4e0f9d0e3a8559d78d43583707e5
Tony Troeff <t.troeff@onebitsoftware.net>
2021-04-13 17:54:31 +0300

    Every identity picker is referenced by a unique identifier.

--- src/SingleValueInputElements/IdentityPickerInputElement/IdentityPicker.tsx
+++ src/SingleValueInputElements/IdentityPickerInputElement/IdentityPicker.tsx
@@ -6,31 +6,39 @@ import { ISingleValueInputElementProps } from '../ISingleValueInputElementProps'
 export class IdentityPicker<TIdentity extends IPersonaProps = IPersonaProps> extends React.Component<
     IIdentityPickerProps<TIdentity> & ISingleValueInputElementProps<TIdentity[]>
 > {
+    private static componentId = 'tas-identity-picker-wrapper';
+
     public render(): JSX.Element {
         if (!this.props) return null;
 
         return (
             <>
-                {this.props.label && <Label required={this.props.renderRequiredIndicator}>{this.props.label}</Label>}
-                <NormalPeoplePicker
-                    onResolveSuggestions={this.props.onResolveSuggestions}
-                    onEmptyResolveSuggestions={this.props.onEmptyResolveSuggestions}
-                    getTextFromItem={this.props.getTextFromItem}
-                    pickerSuggestionsProps={this.props.suggestionProps}
-                    onRemoveSuggestion={this.props.onRemoveSuggestion}
-                    onInputChange={this.props.onInputChange}
-                    resolveDelay={this.props.resolveDelay}
-                    itemLimit={this.props.itemLimit}
-                    disabled={this.props.isDisabled}
-                    defaultSelectedItems={this.props.value}
-                    inputProps={{
-                        required: this.props.renderRequiredIndicator,
-                        placeholder: !!this.props.value && this.props.value.length > 0 ? '' : this.props.placeholder
-                    }}
-                    onChange={(items?: TIdentity[]): void => {
-                        this.props.onChange(items);
-                    }}
-                />
+                {this.props.label && (
+                    <Label htmlFor={IdentityPicker.componentId} required={this.props.renderRequiredIndicator}>
+                        {this.props.label}
+                    </Label>
+                )}
+                <div id={IdentityPicker.componentId}>
+                    <NormalPeoplePicker
+                        onResolveSuggestions={this.props.onResolveSuggestions}
+                        onEmptyResolveSuggestions={this.props.onEmptyResolveSuggestions}
+                        getTextFromItem={this.props.getTextFromItem}
+                        pickerSuggestionsProps={this.props.suggestionProps}
+                        onRemoveSuggestion={this.props.onRemoveSuggestion}
+                        onInputChange={this.props.onInputChange}
+                        resolveDelay={this.props.resolveDelay}
+                        itemLimit={this.props.itemLimit}
+                        disabled={this.props.isDisabled}
+                        defaultSelectedItems={this.props.value}
+                        inputProps={{
+                            required: this.props.renderRequiredIndicator,
+                            placeholder: !!this.props.value && this.props.value.length > 0 ? '' : this.props.placeholder
+                        }}
+                        onChange={(items?: TIdentity[]): void => {
+                            this.props.onChange(items);
+                        }}
+                    />
+                </div>
                 {!!this.props?.errorMessage && <MessageBar messageBarType={MessageBarType.warning}>{this.props.errorMessage}</MessageBar>}
             </>
         );
