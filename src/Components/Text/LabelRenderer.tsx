a0db39e74155151836628dbad049d1a8b45c8930
Tony Troeff <tony13troev@gmail.com>
2022-05-04 16:26:03 +0300

    Fixed the label renderer implementation

--- src/Components/Text/LabelRenderer.tsx
+++ src/Components/Text/LabelRenderer.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Label } from '@fluentui/react';
+import { FormTextRenderer } from './FormTextRenderer';
 import { FormText } from './Types';
 
 interface ILabelRendererProps {
@@ -12,7 +13,11 @@ const LabelRendererComponent = (props: ILabelRendererProps): JSX.Element => {
 
     const { label, required } = props;
     if (!label) return null;
-    return <Label required={required}>{label}</Label>;
+    return (
+        <Label required={required}>
+            <FormTextRenderer text={label} />
+        </Label>
+    );
 };
 
 export const LabelRenderer = React.memo(LabelRendererComponent);
